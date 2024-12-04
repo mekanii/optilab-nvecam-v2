86609704e3358679116c59960b58a220c3be349a
mekanii <margabudi00@gmail.com>
2024-12-04 09:01:58 +0700

    modified:   server.py 	modified:   static/main.js 	modified:   templates/index.html

--- static/main.js
+++ static/main.js
@@ -3,30 +3,30 @@ let pc = new RTCPeerConnection();
 
 // Function to send an offer request to the server
 async function createOffer() {
-    console.log("Sending offer request");
+  console.log("Sending offer request");
 
-    // Fetch the offer from the server
-    const offerResponse = await fetch("/offer", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            sdp: "",
-            type: "offer",
-        }),
-    });
+  // Fetch the offer from the server
+  const offerResponse = await fetch("/offer", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      sdp: "",
+      type: "offer",
+    }),
+  });
 
-    // Parse the offer response
-    const offer = await offerResponse.json();
-    console.log("Received offer response:", offer);
+  // Parse the offer response
+  const offer = await offerResponse.json();
+  console.log("Received offer response:", offer);
 
-    // Set the remote description based on the received offer
-    await pc.setRemoteDescription(new RTCSessionDescription(offer));
+  // Set the remote description based on the received offer
+  await pc.setRemoteDescription(new RTCSessionDescription(offer));
 
-    // Create an answer and set it as the local description
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
+  // Create an answer and set it as the local description
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
 }
 
 // Trigger the process by creating and sending an offer
